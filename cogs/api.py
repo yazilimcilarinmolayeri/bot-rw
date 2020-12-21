43b36fbe1ce5dc637a187a67ff85035e97544be2
cCcTux <ymy.git@protonmail.com>
2020-12-21 23:21:58 +0300

    pypi command added

--- cogs/api.py
+++ cogs/api.py
@@ -17,13 +17,13 @@ class API(commands.Cog):
     async def quake(self, ctx, last=1):
         """
         """
-        
+
         embeds = []
-        
+
         async with self.bot.session.get(
             "https://api.berkealp.net/kandilli/index.php",
-            params={"last": last}
-                ) as resp:
+            params={"last": last},
+        ) as resp:
             if resp.status != 200:
                 return await ctx.send("No data found :(")
             data = await resp.json()
@@ -42,6 +42,32 @@ class API(commands.Cog):
         menu = menus.MenuPages(
             timeout=30,
             clear_reactions_after=True,
-            source=paginator.EmbedSource(data=embeds)
+            source=paginator.EmbedSource(data=embeds),
         )
         await menu.start(ctx)
+
+    @commands.command()
+    async def pypi(self, ctx, package):
+        async with self.bot.session.get(
+            f"https://pypi.org/pypi/{package}/json"
+        ) as resp:
+            if resp.status != 200:
+                return await ctx.send("No package found :(")
+            data = await resp.json()
+            data = data["info"]
+
+        try:
+            project_urls = "\n".join(
+                [f"[{t}]({u})" for t, u in data["project_urls"].items()]
+            )
+        except AttributeError:
+            project_urls = "~"
+
+        embed = discord.Embed(color=self.bot.color)
+        embed.url = data["package_url"]
+        embed.title = data["name"] + " | " + data["version"]
+        embed.description = data["summary"] or "~"
+        embed.add_field(name="Geliştirici", value=data["author"] or "~")
+        embed.add_field(name="Lisans", value=data["license"] or "~")
+        embed.add_field(name="Bağlantılar", value=project_urls)
+        await ctx.send(embed=embed)
