fda066c73fe7fb9e550b5a713a3c6d5163431bf7
cCcTux <ymy.git@protonmail.com>
2021-05-10 22:32:35 +0300

    Added lists.py

---
+++ utils/lists.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+profile_titles = [
+    "İşletim Sistemi",
+    "Masaüstü Ortamı",
+    "Masaüstü Temaları",
+    "Web Tarayıcısı",
+    "Kod/Metin Editörleri",
+    "Terminal Yazılımı",
+    "Kabuk Yazılımı",
+]
+
+profile_questions = [
+    "Favori işletim sistemin nedir?",
+    "Favori masaüstü ortamın nedir?",
+    "Favori pencere/ikon/fare teman nedir?",
+    "Favori web tarayıcının nedir?",
+    "Favori kod/metin editörlerin nedir?",
+    "Favori terminal yazılımın nedir?",
+    "Favori kabuk yazılımın nedir?",
+    "Ekran görüntüsü? Bağlantı olarak ver.",
+]
