1bd2ce476d9f173f7adab4bef5e4124f1b2cf60a
cCcTux <ymy.git@protonmail.com>
2021-05-19 22:44:23 +0300

    Update lists.py

--- utils/lists.py
+++ utils/lists.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+symbols = {
+    "info": "<:infosymbol:844654258301042698>",
+    "success": "<:successsymbol:844654259383697439>",
+    "warning": "<:warningsymbol:844654259387367436>",
+    "error": "<:errorsymbol:844654258455969823>",
+    "loading": "<:loadingsymbol:844654259593150547>",
+    "link": "<:linksymbol:844654258510102580>",
+}
+
 profile_titles = [
     "İşletim Sistemi",
     "Masaüstü Ortamı",
