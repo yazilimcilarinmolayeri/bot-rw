728c956fbd2dc7873195aaa3c12aea9d1f2eb64b
cCcTux <ymy.git@protonmail.com>
2021-05-19 22:44:10 +0300

    Update cotext.py

--- utils/context.py
+++ utils/context.py
@@ -1,23 +1,43 @@
-import discord
+# -*- coding: utf-8 -*-
+
+from utils import lists
 from discord.ext import commands
 
 
 class Context(commands.Context):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.symbols = lists.symbols
 
     def __repr__(self):
         return "<Context>"
 
-    async def tick(self, opt, label):
-        lookup = {
-            True: "<:greentick:844255173085364234>",
-            False: "<:redtick:844255173450006549>",
-            None: "<:graytick:844255172901077052>",
-        }
-        emoji = lookup.get(bool(opt), "<:redtick:844255173450006549>")
+    def _format(self, emoji, content):
+        return "{} | {}".format(emoji, content)
 
-        await self.send("{} | {}".format(emoji, label))
+    async def info(self, content):
+        emoji = self.symbols.get("info")
+        await self.send(self._format(emoji, content))
+
+    async def success(self, content):
+        emoji = self.symbols.get("success")
+        await self.send(self._format(emoji, content))
+
+    async def warning(self, content):
+        emoji = self.symbols.get("warning")
+        await self.send(self._format(emoji, content))
+
+    async def error(self, content):
+        emoji = self.symbols.get("error")
+        await self.send(self._format(emoji, content))
+
+    async def loading(self, content):
+        emoji = self.symbols.get("loading")
+        await self.send(self._format(emoji, content))
+
+    async def link(self, content):
+        emoji = self.symbols.get("link")
+        await self.send(self._format(emoji, content))
 
     async def show_help(self, command=None):
         cmd = self.bot.get_command("help")
