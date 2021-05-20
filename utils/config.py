e2fbe4c4690c778c8794ddbcf89c1b52ef30f472
cCcTux <ymy.git@protonmail.com>
2021-05-20 08:23:33 +0300

    Updated config.py

--- utils/config.py
+++ utils/config.py
@@ -2,19 +2,12 @@
 
 from os import environ as env
 from discord import Status, ActivityType
-from socialscan.util import Platforms as p
 
 
 PREFIX = ["."]
 TOKEN = "" or env.get("TOKEN")
 STATUS_TYPE = Status.idle
 ACTIVITY_TYPE = ActivityType.watching
-
-# =============================================================================
-
-PLATFORMS = [p.TWITTER, p.INSTAGRAM, p.REDDIT, p.GITHUB, p.GITLAB, p.SPOTIFY]
-
-# =============================================================================
 
 DEFAULT_GUILD_ID = 418887354699350028  # YMY
 
