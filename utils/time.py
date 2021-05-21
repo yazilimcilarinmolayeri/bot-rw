6d8ba3e5d6ebb9348998a1f8a738d10f8d21c7a0
cCcTux <ymy.git@protonmail.com>
2021-05-21 19:19:48 +0300

    Update time.py

--- utils/time.py
+++ utils/time.py
@@ -1,30 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import datetime as dt
-
-
-timezone = dt.timezone.utc
+import arrow
 
 
 def day_month_year(value):
-    """
-    Args:
-        value (datetime.datetime): A datetime.
-
-    Returns:
-        tuple: Day, month and year information.
-    """
-
     return (value.day, value.month, value.year)
 
 
-def days_ago(value):
-    """
-    Args:
-        value (datetime.datetime): A datetime.
+def humanize(dt, locale="tr", granularity=["day"]):
 
-    Returns:
-        int: Days information.
-    """
-
-    return (dt.datetime.now(timezone) - value).days
+    return arrow.get(dt).humanize(locale=locale, granularity=granularity)
