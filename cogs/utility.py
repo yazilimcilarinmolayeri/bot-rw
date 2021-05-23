b3d4cea195fae5d3f55d3898d024092cd3fff308
cCcTux <ymy.git@protonmail.com>
2021-05-23 12:21:22 +0300

    Added feedback, perms, botperms command

--- cogs/utility.py
+++ cogs/utility.py
@@ -5,7 +5,7 @@ import time
 import inspect
 import discord
 from discord.ext import commands
-from utils import time as util_time
+from utils import config, time as util_time
 
 
 def setup(bot):
@@ -18,7 +18,7 @@ class Utility(commands.Cog):
 
     @commands.command(aliases=["u"])
     async def uptime(self, ctx):
-        """"""
+        """Tells you how long the bot has been up for."""
 
         await ctx.send(
             "Uptime: `{}`".format(util_time.humanize(self.bot.uptime))
@@ -26,7 +26,7 @@ class Utility(commands.Cog):
 
     @commands.command(aliases=["p"])
     async def ping(self, ctx):
-        """"""
+        """Used to test bot's response time."""
 
         before = time.monotonic()
         message = await ctx.send("Pinging...")
@@ -34,9 +34,93 @@ class Utility(commands.Cog):
 
         await message.edit(content="Pong! `{}ms`".format(round(ping, 2)))
 
+    @commands.command()
+    @commands.cooldown(rate=1, per=60.0, type=commands.BucketType.user)
+    async def feedback(self, ctx, *, content):
+        """Gives feedback about the bot."""
+
+        author = ctx.author
+        channel = self.bot.get_channel(config.FEEDBACK_CHANNEL_ID)
+
+        if channel is None:
+            return
+
+        embed = discord.Embed(
+            color=self.bot.color, timestamp=ctx.message.created_at
+        )
+        embed.set_author(name=author, icon_url=author.avatar.url)
+        embed.description = content
+
+        if ctx.guild is not None:
+            embed.add_field(
+                name="Geri Bildirim Bilgisi",
+                value="Kanal: {} `(ID: {})`\n"
+                "Sunucu: `{} (ID: {})`".format(
+                    ctx.channel.mention,
+                    ctx.channel.id,
+                    ctx.guild,
+                    ctx.guild.id,
+                ),
+            )
+
+        embed.set_footer(text="ID: {}".format(author.id))
+
+        await channel.send(embed=embed)
+        await ctx.send("Geri bildirim gönderildi. Teşekkürler!")
+
+    async def say_permissions(self, ctx, member, channel):
+        allowed, denied = [], []
+        permissions = channel.permissions_for(member)
+
+        embed = discord.Embed(colour=self.bot.color)
+        embed.set_author(name=member, icon_url=member.avatar.url)
+
+        for name, value in permissions:
+            name = name.replace("_", " ").replace("guild", "server").title()
+
+            if value:
+                allowed.append("`{}`".format(name))
+            else:
+                denied.append("`{}`".format(name))
+
+        embed.description = "Allowed: {}\n\nDenied: {}".format(
+            ", ".join(allowed), ", ".join(denied)
+        )
+        embed.set_footer(text="ID: {}".format(member.id))
+
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    async def perms(
+        self,
+        ctx,
+        member: discord.Member = None,
+        channel: discord.TextChannel = None,
+    ):
+        """Shows a member's permissions in a specific channel."""
+
+        channel = channel or ctx.channel
+
+        if member is None:
+            member = ctx.author
+
+        await self.say_permissions(ctx, member, channel)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    async def botperms(self, ctx, *, channel: discord.TextChannel = None):
+        """Shows the bot's permissions in a specific channel."""
+
+        channel = channel or ctx.channel
+        member = ctx.guild.me
+
+        await self.say_permissions(ctx, member, channel)
+
     @commands.command(aliases=["kaynak"])
     async def source(self, ctx, *, command=None):
-        """"""
+        """Displays my full source code or for a specific command."""
 
         branch = "main"
         source_url = "https://github.com/yazilimcilarinmolayeri/ymybot-rw"
