5873ce7d331c9d4b5478ed4919bb08966a3dfebc
Recep Emre Ada <73690406+EmreAda@users.noreply.github.com>
2021-05-26 16:40:25 +0300

    added poll command
    eski bottan kopyaladım, çalışır mı en ufak fikrim yok

--- cogs/utility.py
+++ cogs/utility.py
@@ -35,6 +35,40 @@ class Utility(commands.Cog):
 
         await message.edit(content="Pong! `{}ms`".format(round(ping, 2)))
 
+    @commands.command(aliases=["anket"])
+    async def poll(self, ctx, question, *answers):
+        """
+        Anket oluşturur. En fazla 10 seçenek verebilirsin.
+        
+        Örnek:
+            .poll "Mandalina sever misin ?"
+            .poll "En sevdiğin meyve ?" Elma Armut Mandalina ...
+        """
+
+        embed = discord.Embed(color=self.bot.embed_color)
+        embed.timestamp = datetime.utcnow()
+        embed.title = f"\N{WHITE QUESTION MARK ORNAMENT} {question}"
+        embed.set_footer(text=ctx.author, icon_url=ctx.author.avatar_url)
+
+        if answers == ():
+            msg = await ctx.send(embed=embed)
+            await msg.add_reaction("\N{THUMBS UP SIGN}")
+            await msg.add_reaction("\N{THUMBS DOWN SIGN}")
+            await msg.add_reaction("\N{SHRUG}")
+
+        elif len(answers) <= 10:
+            inner = [f"{i}\u20e3 : {answers[i]}" for i in range(len(answers))]
+            embed.description = "\n".join(inner)
+
+            msg = await ctx.send(embed=embed)
+
+            for i in range(len(answers)):
+                await msg.add_reaction(f"{i}\u20e3")
+
+        else:
+            await ctx.send_help(ctx.command)
+    
+        
     @commands.command()
     @commands.cooldown(rate=1, per=60.0, type=commands.BucketType.user)
     async def feedback(self, ctx, *, content):
