b7812e2f58f7c567b3e3d22c73dbe6430c24df91
ccctux <ymy.git@protonmail.com>
2021-05-29 09:42:43 +0300

    Added models.py

---
+++ utils/models.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from tortoise.models import Model
+from tortoise import Tortoise, fields, run_async
+
+
+class EmojiUsageStat(Model):
+    guild_id = fields.IntField()
+    user_id = fields.IntField()
+    emoji_id = fields.IntField()
+    amount = fields.SmallIntField()
+    last_usage = fields.DatetimeField()
+
+    class Meta:
+        table = "EmojiUsageStat"
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    async def values_list(cls, pk):
+        _ = await cls.get(pk=pk).values_list()
+
+        try:
+            return _[0]
+        except IndexError:
+            return []
+
+
+class Profile(Model):
+    user_id = fields.IntField(pk=True)
+    operation_system = fields.CharField(max_length=255)
+    desktop_environment = fields.CharField(max_length=255)
+    desktop_themes = fields.CharField(max_length=255)
+    web_browser = fields.CharField(max_length=255)
+    code_editors = fields.CharField(max_length=255)
+    terminal_software = fields.CharField(max_length=255)
+    shell_software = fields.CharField(max_length=255)
+    screenshot_url = fields.CharField(max_length=255)
+
+    class Meta:
+        table = "Profile"
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    async def values_list(cls, pk):
+        _ = await cls.get(pk=pk).values_list()
+
+        try:
+            return _[0]
+        except IndexError:
+            return []
+
+
+async def init():
+    await Tortoise.init(
+        db_url="sqlite://database.sqlite3",
+        modules={"models": ["utils.models"]},
+    )
+    await Tortoise.generate_schemas()
