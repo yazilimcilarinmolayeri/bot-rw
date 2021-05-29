ff0b7d5fce6413b4a7c26b78a492ebdf99ca7270
ccctux <ymy.git@protonmail.com>
2021-05-29 09:27:56 +0300

    Added never_gonna_give_you_up_lyrics list

--- utils/lists.py
+++ utils/lists.py
@@ -29,3 +29,12 @@ profile_questions = [
     "Favori kabuk yazılımın nedir?",
     "Ekran görüntüsü? Bağlantı olarak ver.",
 ]
+
+never_gonna_give_you_up_lyrics = [
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you",
+]
