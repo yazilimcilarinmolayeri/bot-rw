823482040471dd332e84da4c1936202f639d9b3b
ccctux <ymy.git@protonmail.com>
2021-06-21 12:04:35 +0300

    bump version...

--- bot.py
+++ bot.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""
+Copyright (C) 2020-2021 yazilimcilarinmolayeri
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import aiohttp
 import discord
 import warnings
@@ -57,7 +74,7 @@ class YMYBOT(commands.Bot):
 
     @property
     def __version__(self):
-        return "0.43.16"
+        return "0.50.20"
 
     async def on_resumed(self):
         print("Resumed...")
