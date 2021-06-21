d576f306af0fff341edaaeaf78acdf14175c089e
ccctux <ymy.git@protonmail.com>
2021-06-21 13:19:08 +0300

    Fix context.py

--- utils/context.py
+++ utils/context.py
@@ -13,40 +13,16 @@ class Context(commands.Context):
     def __repr__(self):
         return "<Context>"
 
-    def _format(self, emoji, content):
-        return "{} | {}".format(emoji, content)
-
     def get_emoji(self, guild, emoji_id):
         emoji = discord.utils.get(guild.emojis, id=emoji_id)
 
-        if emoji.animated:
-            return "<a:{}:{}>".format(emoji.name, emoji.id)
-        else:
-            return "<:{}:{}>".format(emoji.name, emoji.id)
-
-    async def info(self, content):
-        emoji = self.symbols.get("info")
-        await self.send(self._format(emoji, content))
-
-    async def success(self, content):
-        emoji = self.symbols.get("success")
-        await self.send(self._format(emoji, content))
-
-    async def warning(self, content):
-        emoji = self.symbols.get("warning")
-        await self.send(self._format(emoji, content))
-
-    async def error(self, content):
-        emoji = self.symbols.get("error")
-        await self.send(self._format(emoji, content))
-
-    async def loading(self, content):
-        emoji = self.symbols.get("loading")
-        await self.send(self._format(emoji, content))
-
-    async def link(self, content):
-        emoji = self.symbols.get("link")
-        await self.send(self._format(emoji, content))
+        try:
+            if emoji.animated:
+                return "<a:{}:{}>".format(emoji.name, emoji.id)
+            else:
+                return "<:{}:{}>".format(emoji.name, emoji.id)
+        except AttributeError:
+            return False
 
     async def show_help(self, command=None):
         cmd = self.bot.get_command("help")
