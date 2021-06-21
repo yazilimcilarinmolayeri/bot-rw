59f62ab8b3c3c390d9405c7a93e357eebf70aa18
ccctux <ymy.git@protonmail.com>
2021-06-21 14:03:51 +0300

    Add functions.py

---
+++ utils/functions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import re
+import discord
+import mimetypes
+from collections import Counter
+
+
+def list_to_matrix(l, col=10):
+    return [l[i : i + col] for i in range(0, len(l), col)]
+
+
+def is_url_image(url):
+    mimetype, encoding = mimetypes.guess_type(url)
+
+    return mimetype and mimetype.startswith("image")
+
+
+def custom_emoji_counter(guild, message):
+    custom_emojis = Counter(
+        [
+            discord.utils.get(guild.emojis, id=e)
+            for e in [
+                int(e.split(":")[2].replace(">", ""))
+                for e in re.findall(r"<:\w*:\d*>", message.content)
+            ]
+        ]
+    )
+
+    if None in custom_emojis:
+        del custom_emojis[None]
+
+    return custom_emojis
