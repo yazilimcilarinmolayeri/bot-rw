94f2e80a1e425432c487a4d1f5fe20f1b7fa2937
ccctux <ymy.git@protonmail.com>
2021-06-23 13:03:08 +0300

    Fix import module

--- cogs/feed.py
+++ cogs/feed.py
@@ -7,8 +7,8 @@ import discord
 import feedparser
 from io import StringIO
 from datetime import datetime
-from tortoise import tortoise_exceptions
 from discord.ext import commands, tasks, menus
+from tortoise import exceptions as tortoise_exceptions
 from utils import paginator, models, functions, time as util_time
 
 
