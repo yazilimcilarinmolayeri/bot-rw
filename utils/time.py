14541a3ec9ff864756887476f10989c8a399522c
ccctux <ymy.git@protonmail.com>
2021-07-17 12:29:03 +0300

    Add format_dt

--- utils/time.py
+++ utils/time.py
@@ -9,3 +9,10 @@ def day_month_year(value):
 
 def humanize(dt, l="tr", od=False, g="auto"):
     return arrow.get(dt).humanize(locale=l, only_distance=od, granularity=g)
+
+
+def format_dt(dt, style=None):
+    if style is None:
+        return "<t:{}>".format(int(dt.timestamp()))
+
+    return "<t:{}:{}>".format(int(dt.timestamp()), style)
