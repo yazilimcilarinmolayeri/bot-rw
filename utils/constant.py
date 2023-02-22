ad8e0ef5bb9b1dba7066e8bd748fe5722a60745f
lcadyptes <ymy.git@protonmail.com>
2023-02-22 16:22:06 +0300

    General fix

---
+++ utils/constant.py
@@ -0,0 +1,15 @@
+class Badge:
+    administrator = "<:administrator_badge:868205132003823617>"
+    moderator = "<:moderator_badge:868205132519723020>"
+    supporter = "<:supporter_badge:868205132855279746>"
+    olduser = "<:oldmember_badge:868205132779765811>"
+
+
+never_gonna_give_you_up_lyrics = [
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you",
+]
